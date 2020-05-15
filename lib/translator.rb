aad93fa6d96b21c8ae3ceb041ae77b5f792dd7df
Nicole Wang <nicole.wang@yale.edu>
2020-05-15 19:09:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,8 +21,7 @@ def get_english_meaning(path, emoticon)
     meaning[v[:japanese]] = k 
   end 
   
-   meaning[emoticon]
-
+  puts meaning[emoticon]
 end 
 
 def get_japanese_emoticon(path, emoticon)
